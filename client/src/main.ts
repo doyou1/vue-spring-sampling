75e39fd368d54bd820259081e6f5062146d0ef03
doyou1 <tla149@daum.net>
2024-01-08 13:20:50 +0900

    retry

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,8 +4,6 @@ import { VueQueryPlugin } from "vue-query";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 
-export const apiUrlTest = process.env.API_URL_TEST;
-
 const app = createApp(App);
 app.use(VueQueryPlugin);
 app.use(ElementPlus);
